20545fa1367ba4ebf35b35ced126da9dcd81dfe3
LeanKhan <eslean20@gmail.com>
2020-02-10 00:04:14 +0100

    complete 05 book.rb

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,35 @@
 class Book
-# write your code here
+
+# TODO: Check out another way to make instance setters...
+# attr_accessor(:title)
+
+    def initialize
+        # Words that shouldn't be capitalized
+        @dontCapitalize = ["the", "and", "an", "a", "in", "of"]
+    end
+
+    def title
+        @title
+    end
+
+    def title= title
+        words = title.split
+
+        words.map!.with_index do |word, index|
+            capitalize(word, index)
+        end
+
+        @title = words.join(" ")
+    end
+
+    def capitalize word, index
+        
+        if @dontCapitalize.include?(word) && index != 0
+            word
+        else
+            word.capitalize
+        end
+        
+    end
+
 end
