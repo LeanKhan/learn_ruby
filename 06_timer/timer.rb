dcc08dcbbf659cbbec44a9980d4fbe175ce40419
LeanKhan <eslean20@gmail.com>
2020-02-10 21:44:54 +0100

    complete 06_timer, done

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
-  #write your code here
+
+    def initialize
+      @seconds = 0
+    end
+
+    def seconds
+      @seconds
+    end
+
+    def seconds= seconds
+      @seconds = seconds
+    end
+
+    def time_string
+      secs = @seconds % 60
+      mins = ((@seconds - secs) / 60) % 60
+      hours = ((@seconds - mins) / 3600) % 24
+      "#{hours.to_s.rjust(2, '0')}:#{mins.to_s.rjust(2, '0')}:#{secs.to_s.rjust(2, '0')}"
+    end
+
 end
